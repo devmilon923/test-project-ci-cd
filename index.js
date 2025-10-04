e7372193aa6746ce456486d76c4eba4050662429
devmilon <dev.milon923@gmail.com>
2025-10-04 18:32:59 +0600

    added new version

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ mongoose
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  return res.send({ message: "Hey i am from docker conatiner v1" });
+  return res.send({ message: "Hey i am from docker conatiner v2" });
 });
 
 app.listen(PORT, () => {
